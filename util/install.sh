c27c4450aba61b1009708ae19d217130c0e4c608
Ramon Fontes <ramonreisfontes@gmail.com>
2019-02-21 11:52:54 -0300

    update install.sh

--- util/install.sh
+++ util/install.sh
@@ -150,7 +150,7 @@ function mn_deps {
 	fi
 
     echo "Installing Mininet core"
-    pushd $MININET_DIR/mininet-wifi
+    pushd $MININET_DIR/mininet-iot
     if [ -d mininet ]; then
       echo "Removing mininet dir..."
       rm -r mininet
@@ -164,35 +164,35 @@ function mn_deps {
     fi
 
     sudo git clone --depth=1 https://github.com/mininet/mininet.git
-    pushd $MININET_DIR/mininet-wifi/mininet
+    pushd $MININET_DIR/mininet-iot/mininet
     sudo python=${python} make install
     popd
-    echo "Installing Mininet-wifi core"
-    pushd $MININET_DIR/mininet-wifi
+    echo "Installing Mininet-iot core"
+    pushd $MININET_DIR/mininet-iot
     sudo python=${python} make install
     popd
 }
 
-# Install Mininet-WiFi deps
+# Install Mininet-IoT deps
 function wifi_deps {
-    echo "Installing Mininet-WiFi dependencies"
+    echo "Installing Mininet-IoT dependencies"
     $install wireless-tools rfkill ${PYPKG}-numpy pkg-config \
              libnl-3-dev libnl-genl-3-dev libssl-dev make libevent-dev patch \
              ${PYPKG}-scipy ${PYPKG}-matplotlib
 
-	pushd $MININET_DIR/mininet-wifi
+	pushd $MININET_DIR/mininet-iot
     git submodule update --init --recursive
-    pushd $MININET_DIR/mininet-wifi/hostap
-    patch -p0 < $MININET_DIR/mininet-wifi/util/hostap-patches/config.patch
-    pushd $MININET_DIR/mininet-wifi/hostap/hostapd
+    pushd $MININET_DIR/mininet-iot/hostap
+    patch -p0 < $MININET_DIR/mininet-iot/util/hostap-patches/config.patch
+    pushd $MININET_DIR/mininet-iot/hostap/hostapd
     cp defconfig .config
     sudo make && make install
-    pushd $MININET_DIR/mininet-wifi/hostap/wpa_supplicant
+    pushd $MININET_DIR/mininet-iot/hostap/wpa_supplicant
     cp defconfig .config
     sudo make && make install
-    pushd $MININET_DIR/mininet-wifi/
+    pushd $MININET_DIR/mininet-iot/
     git clone --depth=1 https://github.com/ramonfontes/iw
-    pushd $MININET_DIR/mininet-wifi/iw
+    pushd $MININET_DIR/mininet-iot/iw
     sudo make && make install
     cd $BUILD_DIR
     if [ -d mac80211_hwsim_mgmt ]; then
@@ -230,7 +230,7 @@ function of {
     cd $BUILD_DIR/openflow
 
     # Patch controller to handle more than 16 switches
-    patch -p1 < $MININET_DIR/mininet-wifi/util/openflow-patches/controller.patch
+    patch -p1 < $MININET_DIR/mininet-iot/util/openflow-patches/controller.patch
 
     # Resume the install:
     ./boot.sh
@@ -293,7 +293,7 @@ function install_wireshark {
     # Copy coloring rules: OF is white-on-blue:
     echo "Optionally installing wireshark color filters"
     mkdir -p $HOME/.wireshark
-    cp -n $MININET_DIR/mininet-wifi/util/colorfilters $HOME/.wireshark
+    cp -n $MININET_DIR/mininet-iot/util/colorfilters $HOME/.wireshark
 
     echo "Checking Wireshark version"
     WSVER=`wireshark -v | egrep -o '[0-9\.]+' | head -1`
@@ -530,9 +530,9 @@ function nox {
 
     # Apply patches
     git checkout -b tutorial-destiny
-    git am $MININET_DIR/mininet-wifi/util/nox-patches/*tutorial-port-nox-destiny*.patch
+    git am $MININET_DIR/mininet-iot/util/nox-patches/*tutorial-port-nox-destiny*.patch
     if [ "$DIST" = "Ubuntu" ] && version_ge $RELEASE 12.04; then
-        git am $MININET_DIR/mininet-wifi/util/nox-patches/*nox-ubuntu12-hacks.patch
+        git am $MININET_DIR/mininet-iot/util/nox-patches/*nox-ubuntu12-hacks.patch
     fi
 
     # Build
@@ -726,7 +726,7 @@ function wmediumd {
       rm -r wmediumd
     fi
     $install git make libevent-dev libconfig-dev libnl-3-dev libnl-genl-3-dev
-    git clone --depth=1 -b mininet-wifi https://github.com/ramonfontes/wmediumd.git
+    git clone --depth=1 -b mininet-iot https://github.com/ramonfontes/wmediumd.git
     pushd $BUILD_DIR/wmediumd
     sudo make install
     popd
@@ -778,7 +778,7 @@ function all {
     cbench
     wmediumd
     wpan_tools
-    echo "Enjoy Mininet-WiFi!"
+    echo "Enjoy Mininet-IoT!"
 }
 
 # Restore disk space and remove sensitive files before shipping a VM.
@@ -843,7 +843,7 @@ function usage {
     printf -- ' -v: install Open (V)switch\n' >&2
     printf -- ' -V <version>: install a particular version of Open (V)switch on Ubuntu\n' >&2
     printf -- ' -w: install OpenFlow (W)ireshark dissector\n' >&2
-    printf -- ' -W: install Mininet-WiFi dependencies\n' >&2
+    printf -- ' -W: install Mininet-IoT dependencies\n' >&2
     printf -- ' -x: install NO(X) Classic OpenFlow controller\n' >&2
     printf -- ' -y: install R(y)u Controller\n' >&2
     printf -- ' -0: (default) -0[fx] installs OpenFlow 1.0 versions\n' >&2
